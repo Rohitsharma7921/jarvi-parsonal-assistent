a788db6ea731a41e44e99e668664e9a7574b3355
Rohitsharma7921 <108453897+Rohitsharma7921@users.noreply.github.com>
2022-06-30 13:03:37 +0530

    Add files via upload

---
+++ JARVIS Personal Assistant/assistant.h
@@ -0,0 +1,34 @@
+
+/*
+----------------------------------------------------------------------------
+               Project title   : Jarvis(personal assistant)
+               Made By         : Prashant Tyagi
+               Written in      : C++
+               Started project : 22 june , 2022
+               Platform        : Windows
+               IDE used        : Code blocks
+               Last Modified   : 1 june , 2022
+               Total Code      : 700+ Lines
+ ----------------------------------------------------------------------------
+*/
+
+
+
+#ifndef ASSISTANT_H
+#define ASSISTANT_H
+using namespace std;
+
+class assistant
+{
+
+   public:
+              assistant();
+              ~assistant();
+         void greeting();
+         void typing(string);
+         void clock();Aa
+         void speak(string);
+
+};
+
+#endif // ASSISTANT_H
